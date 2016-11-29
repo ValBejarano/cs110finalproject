6b2fe94caccf00d0657d2a80fc2ee27292a443cc
theplaid1 <mrotlev1@binghamton.edu>
2016-11-29 15:33:16 -0500

    Update maingui.py

--- maingui.py
+++ maingui.py
@@ -1,41 +1,115 @@
 from tkinter import *
-import time
 
+
+class Rectangles:
+	
+	def __init__(self,newname):
+		self.x1 = 0
+		self.move_x1 = 0
+		
+		self.y1 = 0
+		self.move_y1 = 0
+		
+		self.x2 = 0
+		self.move_x2 = 0
+		
+		self.y2 = 0
+		self.move_y2 = 0
+		
+		self.color = ""
+		self.rectname = newname
+
+	def draw(self, canvas):
+		self.rectname = canvas.create_rectangle(self.x1, self.y1, self.x2, self.y2, fill=self.color)
+			
 
 def main():
-	master = Tk()
-
-	w = Canvas(master, width=600, height=500)
+	root = Tk()
+	
+	w = Canvas(root, width=600, height=500)
 	w.pack()
+
+	rect1 = Rectangles("rect1")
+	rect1.x1 = 150
+	rect1.y1 = 150
+	rect1.x2 = 220
+	rect1.y2 = 220
+	rect1.color = "blue"
+	rect1.draw(w)
 	
 	
-	colors = ["blue", "red", "green", "orange", "yellow", "purple", "wheat2", "OliveDrab4", "IndianRed3"]
+	rect2 = Rectangles("rect2")
+	rect2.x1 = 220
+	rect2.y1 = 150
+	rect2.x2 = 290
+	rect2.y2 = 220
+	rect2.color = "red"
+	rect2.draw(w)
 	
-	seconds = 0
 	
-
-
-	rect1 = w.create_rectangle(150,150,220,220, fill= colors[seconds])
-	rect2 = w.create_rectangle(220,150,290,220, fill= colors[seconds])
-	rect3 = w.create_rectangle(290,150,360,220, fill= colors[seconds])  
-
-	rect4 = w.create_rectangle(150,220,220,290, fill= colors[seconds])
-	rect5 = w.create_rectangle(220,220,290,290, fill= colors[seconds])
-	rect6 = w.create_rectangle(290,220,360,290, fill= colors[seconds])
-
-	rect7 = w.create_rectangle(150,290,220,360, fill= colors[seconds])
-	rect8 = w.create_rectangle(220,290,290,360, fill= colors[seconds])
-	rect9 = w.create_rectangle(290,290,360,360, fill= colors[seconds])
+	rect3 = Rectangles("rect3")
+	rect3.x1 = 290
+	rect3.y1 = 150
+	rect3.x2 = 360
+	rect3.y2 = 220
+	rect3.color = "green"
+	rect3.draw(w)
 	
-
 	
-	while seconds != 8:
-		print(seconds)
-		time.sleep(1)
-		seconds += 1
-	#w.itemconfig(rect2, fill="blue") # change color
-
-
-	mainloop()
+	rect4 = Rectangles("rect4")
+	rect4.x1 = 150
+	rect4.y1 = 220
+	rect4.x2 = 220
+	rect4.y2 = 290
+	rect4.color = "orange"
+	rect4.draw(w)
 	
+	
+	rect5 = Rectangles("rect5")
+	rect5.x1 = 220
+	rect5.y1 = 220
+	rect5.x2 = 290
+	rect5.y2 = 290
+	rect5.color = "yellow"
+	rect5.draw(w)
+	
+	
+	rect6 = Rectangles("rect6")
+	rect6.x1 = 290
+	rect6.y1 = 220
+	rect6.x2 = 360
+	rect6.y2 = 290
+	rect6.color = "purple"
+	rect6.draw(w)
+	
+	
+	rect7 = Rectangles("rect7")
+	rect7.x1 = 150
+	rect7.y1 = 290
+	rect7.x2 = 220
+	rect7.y2 = 360
+	rect7.color = "pink"
+	rect7.draw(w)
+	
+	
+	rect8 = Rectangles("rect8")
+	rect8.x1 = 220
+	rect8.y1 = 290
+	rect8.x2 = 290
+	rect8.y2 = 360
+	rect8.color = "black"
+	rect8.draw(w)
+	
+	
+	rect9 = Rectangles("rect9")
+	rect9.x1 = 290
+	rect9.y1 = 290
+	rect9.x2 = 360
+	rect9.y2 = 360
+	rect9.color = "white"
+	rect9.draw(w)
+
+
+	root.mainloop()
+
 main()
