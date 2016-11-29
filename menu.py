973051a58e9a3bf0a87c53121dc6463c4a173280
theplaid1 <mrotlev1@binghamton.edu>
2016-11-29 15:40:09 -0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,27 +1,31 @@
 from tkinter import *
 
-class Application(Frame):            #passing in frame from tkinter
+class mainMenu:     
+
     def __init__(self, master):       #main window
-        Frame.__init__(self, master)
-        self.create_widgets()
 
-def openGame():
-   gameWn = Toplevel(root)     #add grid window later and replace with exisiting window
+        self.menuLabel = Label(master, text = "GAME NAME", font = ("Helvetica", 40, "bold"))
+        self.menuLabel.place(x=165, y=100)
+        
+        self.startButton = Button(master, text = "Start", fg = "green", font = ("Helvetica", 17, "bold" ), command = self.openGame)
+        self.startButton.place(x=200, y=300)
 
-def openScoreb():
-   scorebWn = Toplevel(root)      #add scoreboard window later and replace exisiting window
+        scoreboardButton = Button(master, text = "Score Board", fg = "blue", font = ("Helvetica", 17, "bold"), command = self.openScoreb)
+        scoreboardButton.place(x=300, y = 300)
+    
+    def openGame():
+        self.gameWn = Toplevel(root)
+        
 
-root = Tk()                       #can later add more windows within
-root.geometry("450x450+500+300")
-root.title("Grid Game Name(TBD)")
-
-
-menuLabel = Label(root, text = "GAME NAME", font = ("Helvetica", 30, "bold"))
-menuLabel.place(x=100, y=50)
-
-startButton = Button(root, text = "Start", fg = "green", font = ("Helvetica", 15, "bold" ), command = openGame)
-scoreboardButton = Button(root, text = "Score Board", fg = "blue", font = ("Helvetica", 15, "bold"), command = openScoreb)
-startButton.place(x=100, y=150)
-scoreboardButton.place(x=200, y = 150)
-
-root.mainloop()
+    def openScoreb():
+        self.scorebWn = Toplevel(root)     	
+		
+def main():
+		root = Tk()                       
+		menuWn = mainMenu(root)
+		root.geometry("650x650+500+300")
+		root.title("Grid Game Name(TBD)")
+		root.resizable(width=False, height=False)
+		root.mainloop()
+		
+main()
